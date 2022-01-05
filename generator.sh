b4f12d768755fab1553a496cfe0b1576117bd5f7
Thehackingverse <86924237+thehackingverse@users.noreply.github.com>
2022-01-05 15:24:30 +0530

    Update generator.sh

--- generator.sh
+++ generator.sh
@@ -11,7 +11,7 @@ else
 fi 
 fi
 figlet -f slant "mr.generator" | lolcat
-echo "[*]: This script is used for genrating encrypted passwords & decrypt the passwords,hash,identifes the hash [*]
+echo "[*]: This script is used for genrating encrypted passwords & decrypt the passwords,identifes the hash [*]
 [*]: Join our community on discord https://discord.gg/NQgHpCbAmM [*]
 [*]: Copyright© by 2021 thehackingverse[*]"
 #####################################################
